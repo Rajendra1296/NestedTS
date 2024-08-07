d68fa7bce4f5821263e2a85af16ea3320339251f
Rajendra1296 <rajendra.venigalla@proclink.com>
2024-08-07 17:25:21 +0530

    signin validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './users.repository';
 // import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './auth-credentials.dto';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(private userRepository: UserRepository) {}
   async signUp(authCredentialDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialDto);
+  }
+  async signIn(authCredentialDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialDto;
+    const userExists = await this.userRepository.findOne({
+      where: { username },
+    });
+    if (userExists && (await bcrypt.compare(password, userExists.password))) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('please check your login credentials');
+    }
   }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -6,6 +6,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { AuthCredentialsDto } from './auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UserRepository extends Repository<user> {
   constructor(private dataSource: DataSource) {
@@ -13,7 +14,13 @@ export class UserRepository extends Repository<user> {
   }
   async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialDto;
-    const user = this.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const HashedPassword = await bcrypt.hash(password, salt);
+    console.log(password);
+    console.log(salt);
+    console.log(HashedPassword);
+
+    const user = this.create({ username, password: HashedPassword });
     try {
       await this.save(user);
     } catch (error) {
