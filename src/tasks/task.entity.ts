aeba7a0cbd486c927b8c0a1cd85abdd9ed67729d
Rajendra1296e <rajendra.venigalla@proclink.com>
2024-08-08 19:19:03 +0530

    hiding the view of user in task to postman

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task.model';
 import { user } from 'src/auth/user.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class TaskData {
@@ -13,5 +14,6 @@ export class TaskData {
   @Column()
   status: TaskStatus;
   @ManyToOne((_type) => user, (User) => User.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true })
   User: user;
 }
