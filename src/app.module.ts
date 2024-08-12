6a68e30df277bfdfddb867e6d1b8a42580d9996c
Rajendra1296e <rajendra.venigalla@proclink.com>
2024-08-12 16:22:45 +0530

    typeorm config hide details

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,23 +9,29 @@ import { TaskData } from './tasks/task.entity';
 import { TaskRepository } from './tasks/Tasks.repository';
 import { AuthModule } from './auth/auth.module';
 import { UserRepository } from './auth/users.repository';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`],
     }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
-      entities: [TaskData],
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          synchronize: true,
+          autoLoadEntities: true,
+          entities: [TaskData],
+          host: configService.get('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+        };
+      },
     }),
     AuthModule,
   ],
