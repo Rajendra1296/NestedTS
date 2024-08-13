d25e435c599a90ef49613f58332839765540c43c
Rajendra1296e <rajendra.venigalla@proclink.com>
2024-08-13 13:09:20 +0530

    correct way of function testing

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -3,7 +3,7 @@ function addNumbers(num1, num2) {
 }
 describe('addNumbers', () => {
   it('add two numbers', () => {
-    expect(2 + 2).toEqual(addNumbers(2, 2));
+    expect(addNumbers(2, 2)).toEqual(4);
   });
 });
 
