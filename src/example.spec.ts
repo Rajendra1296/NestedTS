dda1c4a5041580ba4e71f00bf54c2c4c5699aa90
Rajendra1296e <rajendra.venigalla@proclink.com>
2024-08-13 13:06:36 +0530

    testing functions

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -1,5 +1,14 @@
-describe('Example test', () => {
-  it('equals true', () => {
-    expect(true).toEqual(true);
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+describe('addNumbers', () => {
+  it('add two numbers', () => {
+    expect(2 + 2).toEqual(addNumbers(2, 2));
   });
 });
+
+// describe('Example test', () => {
+//   it('equals true', () => {
+//     expect(true).toEqual(true);
+//   });
+// });
